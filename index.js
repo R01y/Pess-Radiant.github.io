a0f9012b94287b7b701a5ff28ca0b3a763fa4a7b
Pess-Radiant <109908083+Pess-Radiant@users.noreply.github.com>
2022-07-27 19:40:57 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,1 @@
+window.alert("This website is design for 16:9-3:2 aspect radio user,so if you a phone user please turn browser to desktop site,so I'll recommend use ipad or pc to open this website")
