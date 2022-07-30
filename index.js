83af39c4822916db6b069c88945487f041414356
Ro1y <minecraft92713730@gmail.com>
2022-07-31 07:44:04 +0800

    del

--- index.js
+++
@@ -1,1 +0,0 @@
-window.alert("This website is design for 16:9-3:2 aspect radio user,so if you a phone user please turn browser to desktop site,so I'll recommend use ipad or pc to open this website")
