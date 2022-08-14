0e5576e18b0e3aeb2e1d184e09ae2f1965b3c6ef
Ro1y <minecraft92713730@gmail.com>
2022-08-14 12:40:09 +0800

    8-14

---
+++ index.js
@@ -0,0 +1,20 @@
+//const element = document.getElementById("h1js1")
+//const element = document.getElementById("h2js1")
+//const element = document.getElementById("h1js2")
+
+/*const element = document.getElementById("button1");
+element.onclick = doSomething;
+function doSomething(){
+    const h1_1 = document.getElementById("h1js1");
+    const h2_1 = document.getElementById("h2js1");
+    const h1_2 = document.getElementById("h1js2");
+    
+    h1_1.innerHTML = "歡迎嚟到我哋嘅網站"
+    h1_1.style.fontFamily = " Noto Sans Hong Kong"
+
+    h2_1.innerHTML = "此頁面會係一頁教程&提示頁面<br>亦都代表呢頁會教你點樣使用呢個網站<br>整體好簡單啫<br>同埋你會見到呢個網站有位置係未完成加好美觀<br>使用如果有發現如何漏洞或者錯誤,請喺Instagram聯絡我哋<br>我哋會盡我哋的能力去解決一啲漏洞或者錯誤<br>-------------------------------------------------------"
+    h2_1.style.fontFamily = " Noto Sans Hong Kong"
+
+    h1_2.innerHTML = "教程"
+    h1_2.style.fontFamily = " Noto Sans Hong Kong"
+}*/
